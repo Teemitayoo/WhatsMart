79c367e038b248bf30613e24a5e319e20369d04f
Teemitayoo <teemitayoo5@gmail.com>
2024-01-30 01:26:01 +0100

    zod validation

--- src/modules/auth/auth.dto.ts
+++ src/modules/auth/auth.dto.ts
@@ -7,3 +7,10 @@ export const signupSchema = z.object({
   }),
   storeName: z.string(),
 });
+
+export const signinSchema = z.object({
+  whatsappNumber: z.string().regex(/^\+[1-9]\d{1,14}$/i, {
+    message: 'Invalid WhatsApp number format. Please include the country code, e.g., +123456789',
+  }),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters long' }),
+});
--- src/modules/auth/auth.router.ts
+++ src/modules/auth/auth.router.ts
@@ -3,13 +3,13 @@ import 'express-async-errors';
 import { protect } from '../../common/auth';
 import AuthController from './auth.controller';
 import { validate } from '../../common/zod';
-import { signupSchema } from './auth.dto';
+import { signupSchema, signinSchema } from './auth.dto';
 
 const router = express.Router();
 const authController = new AuthController();
 
 router.post('/signup', validate(signupSchema), authController.signUp);
-router.post('/signin', authController.signIn);
+router.post('/signin', validate(signinSchema), authController.signIn);
 router.post('/logout', protect(true), authController.logout);
 router.post('/add-account-details', protect(true), authController.addAcountDetails);
 router.get('/access-token', authController.getAccessToken);
